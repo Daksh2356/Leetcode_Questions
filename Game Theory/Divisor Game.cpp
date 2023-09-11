dc4c32ada213c0335e1f9611d4b6a480dba70b31
Daksh2356 <dakshmakhija@gmail.com>
2023-09-12 03:03:28 +0530

    New Game Theory folder added

---
+++ Game Theory/Divisor Game.cpp
@@ -0,0 +1,44 @@
+/*
+Question : 1025. Divisor Game
+Date : 11-09-23
+Problem Difficulty level : Easy
+Related Topics : Math, Dynamic Programming, Brainteaser and Game Theory
+
+Problem Statement:
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < n and n % x == 0.
+Replacing the number n on the chalkboard with n - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return true if and only if Alice wins the game, assuming both players play optimally.
+
+
+Example 1:
+Input: n = 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
+
+Example 2:
+Input: n = 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+
+
+Constraints:
+1 <= n <= 1000
+*/
+
+class Solution
+{
+public:
+    bool divisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+
+    // time complexity is O(1)
+    // space complexity is O(1)
+};
