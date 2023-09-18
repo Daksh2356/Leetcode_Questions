7eaffd76a35bad18497af0cf5d5a8b3e8845ca28
Daksh2356 <dakshmakhija@gmail.com>
2023-09-19 02:07:38 +0530

    new matrix-heap question

---
+++ Binary Search/The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,107 @@
+/*
+Question : 1337. The K Weakest Rows in a Matrix
+Date : 18-09-23
+Problem Difficulty level : Easy
+Related Topics :Array, Matrix, Heap( Priority Queue) , Binary Search and Sorting
+Problem Statement:
+
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians).
+The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+    The number of soldiers in row i is less than the number of soldiers in row j.
+    Both rows have the same number of soldiers and i < j.
+    Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+
+Example 1:
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 2
+- Row 1: 4
+- Row 2: 1
+- Row 3: 2
+- Row 4: 5
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+Example 2:
+Input: mat =
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]],
+k = 2
+Output: [0,2]
+Explanation:
+The number of soldiers in each row is:
+- Row 0: 1
+- Row 1: 4
+- Row 2: 1
+- Row 3: 1
+The rows ordered from weakest to strongest are [0,2,3,1].
+
+
+Constraints:
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+
+*/
+class Solution
+{
+public:
+    vector<int> kWeakestRows(vector<vector<int>> &mat, int k)
+    {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<pair<int, int>> myVec;
+        for (int i = 0; i < m; i++)
+        {
+            int s = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (mat[i][j] == 1)
+                    s++;
+            }
+            myVec.emplace_back(i, s);
+        }
+
+        auto compare = [](pair<int, int> a, pair<int, int> b)
+        {
+            if (a.second == b.second)
+            {
+                return a.first < b.first;
+            }
+            else
+            {
+                return a.second < b.second;
+            }
+        };
+
+        sort(myVec.begin(), myVec.end(), compare);
+
+        vector<int> result;
+
+        for (int i = 0; i < k; i++)
+        {
+            result.push_back(myVec[i].first);
+        }
+
+        return result;
+    }
+
+    // time complexity = O(m*log(m))  due to myVec sorting
+    // space complexity = O(m) due to myVec creation
+};
