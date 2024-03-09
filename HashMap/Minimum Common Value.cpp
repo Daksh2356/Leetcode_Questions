c8b8b7492e3dcb68a8509705c9a1998558e63b30
Daksh2356 <dakshmakhija@gmail.com>
2024-03-10 03:05:47 +0530

    new two pointers question added

---
+++ HashMap/Minimum Common Value.cpp
@@ -0,0 +1,58 @@
+/*
+Question : 3005. Minimum Common Value
+Date : 09-03-24
+Problem Difficulty level : Easy
+Related Topics : Array, Hash Table, Two Pointers, Binary Search
+Problem Statement:
+
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. 
+If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+Example 1:
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+Example 2:
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+ 
+Constraints:
+1 <= nums1.length, nums2.length <= 10^5
+1 <= nums1[i], nums2[j] <= 10^9
+Both nums1 and nums2 are sorted in non-decreasing order.
+*/
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else {
+                if (nums1[i] < nums2[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // Time complexity: O(min(m, n))
+    // Space complexity: O(1)
+
+    // If memory usage is a concern and the input arrays are sorted or can be
+    // sorted efficiently, the two-pointer approach is usually preferred.
+    // However, if simplicity and readability are more important, or if the
+    // arrays are not sorted, using a set can still be a viable option.
+};
