a59cccc3662614bb80490aa1d3f10ad71d519169
daksh2356 <dakshmakhija@gmail.com>
2025-05-11 01:02:52 +0530

    Other Platforms Folder Renamed and Added Second Largest Element Solution (GFG)

---
+++ Other Platform Questions (InterviewBit, GFG, etc)/Second Largest.java
@@ -0,0 +1,53 @@
+/*
+ Date : 10/05/25
+ Platform : GFG
+ Track : GFG-160-problems
+
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+?Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+?Output: 34
+*Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+?Output: 5
+*Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+?Output: -1
+*Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
+
+!Constraints:
+!2 ≤ arr.size() ≤ 10^5
+!1 ≤ arr[i] ≤ 10^5
+
+*/
+
+code:
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        
+        int n = arr.length;
+        int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
+        
+        for(int i =0; i<n; i++){
+            if(max < arr[i]){
+                secondMax = max;
+                max = arr[i];
+            }
+            else{
+                if(secondMax < arr[i] && arr[i]!=max)
+                    secondMax = arr[i];
+            }
+        }
+        
+        return secondMax == Integer.MIN_VALUE ? -1 : secondMax;
+    }
+}
+// time complexity = O(n)
+// space complexity = O(1)
