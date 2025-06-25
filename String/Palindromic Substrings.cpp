48995deff59e7c4b88ecceeb68b6e7f7940782f2
daksh2356 <dakshmakhija@gmail.com>
2025-06-26 00:16:37 +0530

    Program to find the number of palindromic substrings in a string.

--- String/Palindromic Substrings.cpp
+++ String/Palindromic Substrings.cpp
@@ -73,5 +73,36 @@ public:
     }
 
     // time = O(n^2)
-    // space = O(n)
+    // space = O(n^2)
+};
+
+
+// another approach (with constant space)
+class Solution {
+public:
+    int expandAroundCenter(string& s, int left, int right) {
+        int count = 0;
+
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            count += expandAroundCenter(s, i, i);     // Odd length
+            count += expandAroundCenter(s, i, i + 1); // Even length
+        }
+
+        return count;
+    }
+    
+    // time = O(n^2) [worst-case]
+    // space = O(1)
 };
