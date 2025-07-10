a01464344a788376cddc82f4142eb9325d1e3311
daksh2356 <dakshmakhija@gmail.com>
2025-07-11 00:25:55 +0530

    Brute force approach to find H-index (Hirsch Index) in a given citations[] array

---
+++ Sorting/Find H-Index.java
@@ -0,0 +1,48 @@
+/*
+Date : 09/07/25
+Platform : GFG
+Track : GFG-160-problems
+Problem Difficulty: Medium
+
+Problem Statement:
+Given an integer array citations[], where citations[i] is the number of citations a researcher received for the ith paper. The task is to find the H-index.
+
+H-Index is the largest value such that the researcher has at least H papers that have been cited at least H times.
+
+? Example 1:
+Input: citations[] = [3, 0, 5, 3, 0]
+Output: 3
+* Explanation: There are at least 3 papers (3, 5, 3) with at least 3 citations.
+
+? Example 2:
+Input: citations[] = [5, 1, 2, 4, 1]
+Output: 2
+* Explanation: There are 3 papers (with citation counts of 5, 2, and 4) that have 2 or more citations. However, the H-Index cannot be 3 because there aren't 3 papers with 3 or more citations.
+
+? Example 3:
+Input: citations[] = [0, 0]
+Output: 0
+
+Constraints:
+! 1 ≤ citations.size() ≤ 10^6
+! 0 ≤ citations[i] ≤ 10^6
+*/
+
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        // code here
+    Integer[] arr = Arrays.stream(citations).boxed().toArray(Integer[] :: new);
+    Arrays.sort(arr, Collections.reverseOrder());
+    
+    int idx = 0 , n = arr.length;
+    while(idx < n && arr[idx] > idx){
+        idx++;
+    }
+    
+    return idx;
+    }
+}
+
+// time complexity = O(nlogn)
+// space complexity = O(n)
